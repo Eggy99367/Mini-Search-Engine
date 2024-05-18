5ff92ed26ff55e812cba74271d57895040072951
Tse-Wen <twchen1258@gmail.com>
2024-05-17 22:52:40 -0700

    add milestone 2

--- a3.py
+++ a3.py
@@ -1,6 +1,68 @@
 from src import *
+from porter2stemmer import Porter2Stemmer
+
+#take two docID list as input, return common docIDs
+def intersect(posting1, posting2):
+    answer = []
+    p1 = 0
+    p2 = 0
+    while p1 != len(posting1) and p2 != len(posting2):
+        if int(posting1[p1]) == int(posting2[p2]):
+            answer.append(posting1[p1])
+            p1 += 1
+            p2 += 1
+        else:
+            if int(posting1[p1]) < int(posting2[p2]):
+                p1 += 1
+            else:
+                p2 += 1
+    return answer
+
 
 if __name__ == "__main__":
     # just run the build_inverted_index once, comment it after you have the index built
-    build_inverted_index("/Users/vince/Desktop/UCI/Sophomore/Spring 2024/ICS 121/Assignment3/Comp121_Assignment3/DEV")
+    if not os.path.exists("all_urls.txt") and not os.path.exists("index.txt") and not os.path.exists("words.txt"):
+        build_inverted_index("/Users/davidchen/inf141/Assignment3/DEV")
+
+    newFetcher = fetcher("all_urls.txt","words.txt")
+    p2s = Porter2Stemmer()
+
+    print("What do you want to search? (If you want to exit, type 'exit')")
+    key = input()
+    while key != "exit":
+        stemmed_tokens = []  # Create a new list of stemmed tokens
+        current_word = []
+        for char in key:
+            if isAlpNum(char):
+                current_word.append(char)
+            elif len(current_word) != 0:
+                word = "".join(current_word).lower()
+                stemmed_tokens.append(p2s.stem(word))  # appending stemmed tokens
+                current_word = []
+        if current_word:
+            word = "".join(current_word).lower()
+            stemmed_tokens.append(p2s.stem(word))
+
+        #get token sorted by freq
+        sortedToken = sorted([(token, newFetcher.get_token_freq(token)) for token in stemmed_tokens], key=(lambda x: x[1]))
+        if len(sortedToken)==1:
+            ids = newFetcher.get_docIds_by_token(sortedToken[0][0])
+            print("The top five urls under this search:")
+            for idNum in ids[:5]:
+                print(newFetcher.get_url_by_id(idNum))
+            print()
+        else:
+            curr_token = sortedToken[0][0]
+            ids = newFetcher.get_docIds_by_token(curr_token)
+            for index in range(1,len(sortedToken)):
+                curr_token = sortedToken[index][0]
+                ids = intersect(ids, newFetcher.get_docIds_by_token(curr_token))
+            # Print the first five url that contains all token
+            print("The top five urls under this search:")
+            for idNum in ids[:5]:
+                print(newFetcher.get_url_by_id(idNum))
+            print()
+        print("What do you want to search? (If you want to exit, type 'exit')")
+        key = input()
+
     # print(get_token_freq("我"))
--- src/fetch.py
+++ src/fetch.py
@@ -1,65 +1,76 @@
 import json, re
 from .posting import *
 
+
 def fetch_urls(path):
     with open(path, 'r') as file:
         return json.loads(file.read())
-    
+
+
 def fetch_token_list(path):
     with open(path, 'r') as file:
         return file.read().split(',')
-    
-urls_dict = fetch_urls("all_urls.txt")
-token_list = fetch_token_list("words.txt")
 
-def fetch_token(record:str):
+
+def fetch_token(record: str):
     return re.search(r'\[(.*?)\]', record).group(1)
 
-def _fetch_postings(record:str):
-    print(record)
+
+def _fetch_postings(record: str):
+    #print(record)
     record = record.strip().split(']')[1]
     posting_txts = record.split('/')
     postings = [str_to_posting(txt) for txt in posting_txts]
     return postings
 
-def get_url_by_id(docId: int | str) -> str:
-    return urls_dict[str(docId)]["url"]
 
-def get_url_encoding_by_id(docId: int | str) -> str:
-    return urls_dict[str(docId)]["encoding"]
+class fetcher:
+    def __init__(self, urls_path, tokens_path):
+        self.urls_dict = fetch_urls(urls_path)
+        self.token_list = fetch_token_list(tokens_path)
 
-def _get_token_line_number(token: str) -> str:
-    return token_list.index(token) + 1
+    # input a docID, return the url
+    def get_url_by_id(self, docId) -> str:
+        return self.urls_dict[str(docId)]["url"]
 
-def _get_line(path, line_number):
-    with open(path, 'r') as file:
-        for line_num, line in enumerate(file, start=1):
-            if line_num == line_number:
-                return line.strip()
-            
-def get_token_line(token):
-    return _get_line("index.txt", _get_token_line_number(token))
+    # input the docID, get the encoding of the doc
+    def get_url_encoding_by_id(self, docId) -> str:
+        return self.urls_dict[str(docId)]["encoding"]
 
-def get_token_freq(token):
-    try:
-        return len(get_token_line(token).split('/'))
-    except Exception:
-        return 0
+    def _get_token_line_number(self, token: str) -> int:
+        return self.token_list.index(token) + 1
 
-def get_postings(token):
-    try:
-        return _fetch_postings(get_token_line(token))
-    except Exception:
-        return []
+    def _get_line(self, path, line_number):
+        with open(path, 'r') as file:
+            for line_num, line in enumerate(file, start=1):
+                if line_num == line_number:
+                    return line.strip()
 
-def get_docIds_by_token(token):
-    try:
-        return [p.docId() for p in get_postings(token)]
-    except Exception:
-        return []
-    
-def get_weights_by_token(token):
-    try:
-        return [p.weight() for p in get_postings(token)]
-    except Exception:
-        return []
+    def get_token_line(self, token):
+        return self._get_line("index.txt", self._get_token_line_number(token))
+
+    def get_token_freq(self, token):
+        try:
+            return len(self.get_token_line(token).split('/'))
+        except Exception:
+            return 0
+
+    def get_postings(self, token):
+        try:
+            return _fetch_postings(self.get_token_line(token))
+        except Exception:
+            return []
+
+
+    def get_docIds_by_token(self, token):
+        try:
+            return [p.docId() for p in self.get_postings(token)]
+        except Exception:
+            return []
+
+
+    def get_weights_by_token(self, token):
+        try:
+            return [p.weight() for p in self.get_postings(token)]
+        except Exception:
+            return []
